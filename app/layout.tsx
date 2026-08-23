076ad16939f2373acb98100b60601b75168b3d40
Seojkc <seojameskc007@gmail.com>
2026-08-23 17:47:45 -0400

    update: updated the icon of the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import logo from "../public/Assets/logo-portfolio.png"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Seo James",
   description: "Designed by Seo James",
+  icons: {
+    icon: '/Assets/icon-5.png',
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +32,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
